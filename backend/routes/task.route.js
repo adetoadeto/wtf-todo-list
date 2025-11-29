c118c3ea54b809bef59242a0268a51b4a6bfe055
adetoadeto <157330742+adetoadeto@users.noreply.github.com>
2025-11-29 12:31:14 +0100

    user authentication middleware added to task routes

--- backend/routes/task.route.js
+++ backend/routes/task.route.js
@@ -1,17 +1,19 @@
 import express from "express";
 import {createTask, getTaskById, updateTaskById, getTaskByStatus, updateTasks, deleteTask, getAllTasks} from "../controllers/task.controller.js";
 
+import verifyUser from "../middlewares/verifyUser.js";
+
 const router = express.Router();
 
 // get all tasks (use user Id to get all his tasks)
 
-router.post("/create", createTask)
-router.get("/all-tasks", getAllTasks)
-router.get("/:id", getTaskById)
-router.get("/sort/:status", getTaskByStatus)
-router.patch("/update/:id", updateTaskById)
-router.patch("/status-update/:status", updateTasks)
-router.delete("/delete", deleteTask)
+router.post("/create", verifyUser, createTask)
+router.get("/all-tasks", verifyUser, getAllTasks)
+router.get("/:id", verifyUser, getTaskById)
+router.get("/sort/:status", verifyUser, getTaskByStatus)
+router.patch("/update/:id", verifyUser, updateTaskById)
+router.patch("/status-update/:status", verifyUser, updateTasks)
+router.delete("/delete", verifyUser, deleteTask)
 
 export default router;
 
--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,6 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
-import verifyUser from "../middlewares/verifyUser.js";
-
 import taskRoute from "./routes/task.route.js";
 import userRoute from "./routes/user.route.js";
 import authRoute from "./routes/auth.route.js";
@@ -27,7 +25,7 @@ app.use(cors({
 }));
 
 app.use(express.json())
-app.use("/api/task", verifyUser, taskRoute)
+app.use("/api/task", taskRoute)
 app.use("/api/user", userRoute)
 app.use("/api/auth", authRoute)
 
